3b5ae10b8be6f4f9ce1c13157d7259091d7c976d
RichardJW1 <g13w8093@campus.ru.ac.za>
2018-02-28 19:33:11 +0200

    Commenting and initialization

--- Morabaraba/Program.fs
+++ Morabaraba/Program.fs
@@ -4,16 +4,20 @@ open System
 
 
 
-(*Random Notes about Program:
-    24 possible Possitios
-    20 possible rows of 3 cows
-    8 Possitions with the possiblity of 4 cows next to them, the rest can only have 3
+(*
+//--------------------Program Info--------------------
+
+    Random Notes about Game:
+        24 possible Possitions
+        20 possible rows of 3 cows
+        8 Possitions with the possiblity of 4 cows next to them, the rest can only have 3
 
 
 *)
 //VVVVVVVVVVVVV//
 
 module GameSession =      //Corbyn: basic idea of a game skeleton
+    open System.Security.Policy
  
     type Board = {
                 
@@ -25,14 +29,15 @@ module GameSession =      //Corbyn: basic idea of a game skeleton
     type Cow = {
 
         Position : char*int  // store position has letter and number, ie: (A,3)
-        isFlyingCow : bool   
+        isFlyingCow : bool 
+        isAlive : bool        // Richard: True if Alive
     }
 
     type CowList = {
         
         hasThree : bool     // To now when cows can fly
         hasTwo : bool       // Player has lost
-        Cows : CowList
+        Cows : Cow List          // Richard: Was this meant to be CowList or Cow List? I changed it to Cow List
     }
 
     type Player = {
@@ -42,20 +47,115 @@ module GameSession =      //Corbyn: basic idea of a game skeleton
         isWinner : bool     // Player has won
         Cows : CowList       
     }
+    
     //let nextTurn =
     //let gameOver =
 
-//^^^^^^^^^^^^//
-module Players = 
-    
-    type Character= {
-        Name : string
-        CurrentPos : int List
-        RemainingCows : int         //Just messing around with an idea...
+
+
+
+//--------------------Initialize board--------------------
+
+    let theBoard = {
+        board = [
+                "             1   2   3   4   5   6   7 " //Line 0
+                "        A   ( )---------( )---------( )"   //Line 1
+                "             | \         |         / |"    //Line 2
+                "        B    |  ( )-----( )-----( )  |"    //Line 3
+                "             |   | \     |     / |   |"    //Line 4
+                "        C    |   |  ( )-( )-( )  |   |"    //Line 5
+                "             |   |   |       |   |   |"    //Line 6
+                "        D   ( )-( )-( )     ( )-( )-( )"   //Line 7
+                "             |   |   |       |   |   |"    //Line 8
+                "        E    |   |  ( )-( )-( )  |   |"    //Line 9
+                "             |   | /     |     \ |   |"    //Line 10
+                "        F    |  ( )-----( )-----( )  |"    //Line 11
+                "             | /         |         \ |"    //Line 12
+                "        G   ( )---------( )---------( )"]  //Line 13
     }
     
+//--------------------Initialize Cows--------------------
+
+    let Cow = {
+        
+        Position = ('Z',-1)
+        isFlyingCow = false
+        isAlive = true
+    }
+
+    let listOfCows = {
+        
+        hasThree = false
+        hasTwo = false
+        Cows = [Cow;Cow;Cow;Cow;Cow;Cow;Cow;Cow;Cow;Cow;Cow;Cow]    // 12 Cows 
+    }
+
+//--------------------Initialize Players-------------------- 
+
+    let player1 = {
+        
+        Id = 1
+        IsPlaying = true
+        isWinner = false
+        Cows = listOfCows 
+    }
+
+    let player2 = {
+        
+        Id = 2
+        IsPlaying = true
+        isWinner = false
+        Cows = listOfCows 
+    }
+
+    
+//--------------------Console Properties--------------------
+
+Console.ForegroundColor <- ConsoleColor.Green 
 
 
+//--------------------Let The Games Begin--------------------
+open GameSession
+
+//Test
+//Console.WriteLine player2.Id
+
+
+printfn "%A" theBoard.board     // print the board
+
+
+
+
+
+
+
+
+
+
+Console.ReadKey()       //Just to keep the console open
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------------------RANDOM BITS OF CODE REMOVED------------------------------
+
+(*
 Console.ForegroundColor <- ConsoleColor.Green 
 
 Console.WriteLine "             1   2   3   4   5   6   7"
@@ -76,30 +176,20 @@ Console.WriteLine "        G   ( )---------( )---------( )"
 
 
 
-let bored = [
-    "             1   2   3   4   5   6   7 \n" 
-    "        A   ( )---------( )---------( )"   //Line 1
-    "             | \         |         / |"    //Line 2
-    "        B    |  ( )-----( )-----( )  |"    //Line 3
-    "             |   | \     |     / |   |"    //Line 4
-    "        C    |   |  ( )-( )-( )  |   |"    //Line 5
-    "             |   |   |       |   |   |"    //Line 6
-    "        D   ( )-( )-( )     ( )-( )-( )"   //Line 7
-    "             |   |   |       |   |   |"    //Line 8
-    "        E    |   |  ( )-( )-( )  |   |"    //Line 9
-    "             |   | /     |     \ |   |"    //Line 10
-    "        F    |  ( )-----( )-----( )  |"    //Line 11
-    "             | /         |         \ |"    //Line 12
-    "        G   ( )---------( )---------( )"]  //Line 13
+
+module Players = 
+    
+    type Character= {
+        Name : string
+        CurrentPos : int List
+        RemainingCows : int         //Just messing around with an idea...
+    }
 
 
 
 
 
-Console.ReadKey()       //Just to keep the console open
 
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+
+*)
